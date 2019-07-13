de897b760013fc69d210df2a0c41245efe78d438
zjingqi <1548179019@qq.com>
2019-07-13 17:52:35 +0800

    Site updated: 2019-07-13 17:52:34

---
+++ photoList/index.js
@@ -0,0 +1,14 @@
+var timer = setTimeout(function(){
+    $('.wrapper').removeClass('init');
+
+},200)
+
+$('.item').click(function(){
+    $(this).addClass('active');
+    $('.wrapper').addClass('wrapper-active');
+}) 
+$('.close').click(function(e){
+    e.stopPropagation();/* 取消冒泡*/
+    $('.wrapper').removeClass('wrapper-active');
+    $('.active').removeClass('active');
+})
